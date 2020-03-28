64d7f171f618209b33f2fbf13b0533f7ea08f1f1
Joshal Daftari <joshal@outlook.com>
2020-03-28 13:12:14 -0700

    Add Brewfile for brew bundle

---
+++ setup_mac.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -x
+
+if [ "$(uname)" != "Darwin" ]; then
+    echo "Unable to establish that this is Mac. Skipping..."
+fi
+
+echo "Setting up your Mac..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle -v
+
+# Make homebrew installed zsh the default shell
+if ! grep -Fxq "/usr/local/bin/zsh" /etc/shells; then
+    echo "/usr/local/bin/zsh" | sudo tee -a /etc/shells
+fi
+
+chsh -s /usr/local/bin/zsh
